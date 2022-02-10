ebec0bbdac70bba823b877444157d1d707cce622
David Stone <davidjamesstone@gmail.com>
2022-02-10 19:26:47 +0000

    Geojson RFC7946

--- bin/shp2geojson.sh
+++ bin/shp2geojson.sh
@@ -16,6 +16,6 @@ fi
 for (( i=0; i<$count; i++ )); do
    echo "Processing feature #$i..."
    filename=$(ogrinfo -fid $i -ro -noextent -nomd -geom=NO "$1" "$layername" | grep "$2 " | awk '{print $4}')
-   ogr2ogr -f GeoJSON -t_srs EPSG:4326 -sql "SELECT fws_tacode AS code, ta_name AS name, descrip AS description, area AS region from $layername" -fid "$i" -lco COORDINATE_PRECISION=6 "$3/$filename.json" "$1"
+   ogr2ogr -f GeoJSON -t_srs EPSG:4326 -sql "SELECT fws_tacode AS code, ta_name AS name, descrip AS description, area AS region from $layername" -fid "$i" -lco RFC7946=YES -lco COORDINATE_PRECISION=6 "$3/$filename.json" "$1"
 done
 
